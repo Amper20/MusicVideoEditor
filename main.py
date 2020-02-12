e06d3209003b7043228afc5f956af069ef0a6b2d
Amper20 <butmalai.dan@gmail.com>
2020-02-12 18:04:51 +0200

    tested mido library, looks intuitive might use in future

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("ahuencik")
+import sys
+import mido
+import time
+from mido import MidiFile
+
+filename = sys.argv[1]
+midifile = MidiFile('blr.mid')
+for i, track in enumerate(midifile.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    for msg in track:
+        print(msg)
+
