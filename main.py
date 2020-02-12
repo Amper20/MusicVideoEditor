ca524fc5998dce949af10dc2f86446996d59651e
Amper20 <butmalai.dan@gmail.com>
2020-02-12 18:04:51 +0200

    i am not sure if it is mido library or my programming skills but something is not good

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ import time
 from mido import MidiFile
 
 filename = sys.argv[1]
-midifile = MidiFile('blr.mid')
-for i, track in enumerate(midifile.tracks):
-    print('Track {}: {}'.format(i, track.name))
-    for msg in track:
-        print(msg)
+import pygame.midi
 
+
+pygame.midi.init()
+
+print (pygame.midi.get_default_output_id())
+print (pygame.midi.get_device_info(0))
+
+player = pygame.midi.Output(0)
+
+player.set_instrument(0)
+
+
+midifile = MidiFile('test.mid', clip=True)
+print(midifile)
+for msg in midifile.play():
+    if msg.type == 'note_on' or msg.type == 'note_off':
+            print(msg)
+            player.note_on(msg.note, 127)
+            time.sleep(msg.time)
+
+pygame.midi.quit()
