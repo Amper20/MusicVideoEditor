4a04001e7b260444dbf2c7b9daa4226b0b99ae6e
Amper20 <butmalai.dan@gmail.com>
2020-02-14 15:28:06 +0200

    it was the progaming skill things start to make sense

--- main.py
+++ main.py
@@ -2,27 +2,40 @@ import sys
 import mido
 import time
 from mido import MidiFile
-
-filename = sys.argv[1]
 import pygame.midi
 
+tempo = 500000  # equivalent to 120 bpm
 
-pygame.midi.init()
+# type 0 (single track): all messages are saved in one track
+def type0(midifile):
+    assert isinstance(midifile, MidiFile)
 
-print (pygame.midi.get_default_output_id())
-print (pygame.midi.get_device_info(0))
+# type 1 (synchronous): all tracks start at the same time
+def type1(midifile):
+    assert isinstance(midifile, MidiFile)
+    for track in midifile.tracks:
+        print('---------------')
+        print(track)
+        print('---------------')
+        for msg in track:
+            print(msg)
 
-player = pygame.midi.Output(0)
 
-player.set_instrument(0)
+# type 2 (asynchronous): each track is independent of the others
+def type2(midifile):
+    assert isinstance(midifile, MidiFile)
 
+# pygame.midi.init()
+#
+# print(pygame.midi.get_default_output_id())
+# print(pygame.midi.get_device_info(0))
+#
+# player = pygame.midi.Output(0)
+#
+# player.set_instrument(0)
 
 midifile = MidiFile('test.mid', clip=True)
-print(midifile)
-for msg in midifile.play():
-    if msg.type == 'note_on' or msg.type == 'note_off':
-            print(msg)
-            player.note_on(msg.note, 127)
-            time.sleep(msg.time)
+playType = {0: type0, 1: type1, 2: type2}
+playType[midifile.type](midifile)
 
-pygame.midi.quit()
+# pygame.midi.quit()
